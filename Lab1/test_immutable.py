ad1903ae434711e22123d1a70df42d17e7baebc1
Yangkang <792131173@qq.com>
2020-05-06 23:53:03 +0800

    yangkang commit

---
+++ Lab1/test_immutable.py
@@ -0,0 +1,62 @@
+import unittest
+from hypothesis import given
+import hypothesis.strategies as st
+from lab1.immutable_version import *
+
+class TestMutableList(unittest.TestCase):
+
+    def test_size(self):
+        self.assertEqual(size(MyHashMap(vItem=[])), 0)
+        self.assertEqual(size(MyHashMap(vItem=[3, 8])), 2)
+        self.assertEqual(size(MyHashMap(vItem=[6, 3, 4])), 3)
+
+    def test_extend(self):
+        self.assertEqual(extend(MyHashMap(vItem=[]),2).to_list().sort(),[2].sort())
+        self.assertEqual(extend(MyHashMap(vItem=[3,67,1]), 2).to_list().sort(), [3,67,1,2].sort())
+        self.assertEqual(extend(MyHashMap(vItem=[2,3]), 66).to_list().sort(), [2,3,66].sort())
+
+    def test_remove(self):
+        self.assertEqual(remove(MyHashMap(vItem=[2]),2).to_list().sort(),[].sort())
+        self.assertEqual(remove(MyHashMap(vItem=[3,6,8]), 2).to_list().sort(), [3,6,8].sort())
+        self.assertEqual(remove(MyHashMap(vItem=[24,5,2]), 5).to_list().sort(), [24,2].sort())
+
+    def test_to_list(self):
+        self.assertEqual(to_list(MyHashMap(vItem=[3,6,8])).sort(),[3,6,8].sort())
+        self.assertEqual(to_list(MyHashMap(vItem=[17,8])).sort(), [17,8].sort())
+
+    def test_mconcat(self):
+        self.assertEqual(mconcat(MyHashMap(vItem=[22,1,2]),MyHashMap(vItem=[22,5,6])).to_list().sort(),[22,1,2,5,6].sort())
+        self.assertEqual(mconcat(MyHashMap(vItem=[]),MyHashMap(vItem=[22,5,6])).to_list().sort(), [22, 5, 6].sort())
+
+    def test_mempty(self):
+        self.assertEqual(mempty(),None)
+
+    @given(st.lists(st.integers()))
+    def test_from_list_to_list_equality(self, a):
+        lst = MyHashMap()
+        lst.from_list(a)
+        self.assertEqual(to_list(lst).sort(),a.sort())
+
+    @given(st.lists(st.integers()))
+    def test_monoid_identity(self, lst):
+        a = MyHashMap()
+        a.from_list(lst)
+        self.assertEqual(mconcat(mempty(),a), a)
+        self.assertEqual(mconcat(a,mempty()), a)
+
+    # def test_iter(self):
+    #     x=[1,2,3]
+    #     lst = MyHashMap()
+    #     lst.from_list(x)
+    #     tmp=[]
+    #     try:
+    #         get_next = iterator(lst)
+    #         while True:
+    #             tmp.append(get_next)
+    #     except StopIteration:
+    #         pass
+        # self.assertEqual(x.sort(),tmp.sort())
+        # self.assertEqual(to_list(lst).sort(),tmp)
+
+if __name__=='__main__':
+    unittest.main()
---
+++ Lab1/test_mutable.py
@@ -0,0 +1,112 @@
+import unittest
+from hypothesis import given
+import hypothesis.strategies as st
+from lab1.mutable_version import *
+
+class TestMutableList(unittest.TestCase):
+
+    def test_size(self):
+        self.assertEqual(MyHashMap(vItem=[]).size(),0)
+        self.assertEqual(MyHashMap(vItem=[3,8]).size(), 2)
+        self.assertEqual(MyHashMap(vItem=[6,3,4]).size(), 3)
+
+    def test_to_list(self):
+        self.assertEqual(MyHashMap(vItem=[]).to_list(),[])
+        self.assertEqual(MyHashMap(vItem=[6]).to_list(), [6])
+        self.assertEqual(MyHashMap(vItem=[5,77]).to_list(), [5,77])
+
+    def test_from_list(self):
+        test_data =[
+            [],
+            ['a'],
+            ['a','b']
+        ]
+        for e in test_data:
+            lst = MyHashMap(vItem=[])
+            lst.from_list(e)
+            self.assertEqual(lst.to_list().sort(),e.sort())
+
+    def test_insert(self):
+        lst = MyHashMap(vItem=[])
+        lst.insert(2)
+        self.assertEqual(lst.to_list().sort(), [2].sort())
+        lst.insert(55)
+        self.assertEqual(lst.to_list().sort(),[2,55].sort())
+
+    def test_remove(self):
+        lst = MyHashMap(vItem=[2,4,7])
+        lst.remove(2)
+        self.assertEqual(lst.to_list().sort(),[4,7].sort())
+        lst = MyHashMap(vItem=[55, 16, 7])
+        lst.remove(2)
+        self.assertEqual(lst.to_list().sort(), [55, 16, 7].sort())
+
+
+    def test_find(self):
+        lst = MyHashMap(vItem=[2,4,7])
+        result =lst.find(4)
+        self.assertEqual(result,True)
+        result = lst.find(5)
+        self.assertEqual(result, False)
+        result = lst.find(7)
+        self.assertEqual(result, True)
+
+    def test_filter(self):
+        lst = MyHashMap(vItem=[1,2,3,4,5,6,7,8,9])
+        lst.filter(lambda x:x%2==0)
+        self.assertEqual(lst.to_list().sort(),[2,4,6,8].sort())
+        lst = MyHashMap(vItem=[1, 2, 3, 4, 5, 6, 7, 8, 9])
+        lst.filter(lambda x: x % 2 != 0)
+        self.assertEqual(lst.to_list().sort(), [1,3,5,7,9].sort())
+
+    def test_map(self):
+        lst = MyHashMap(vItem=[])
+        lst.map(str)
+        self.assertEqual(lst.to_list(),[])
+        lst = MyHashMap(vItem=[1,2,3,4,5])
+        lst.map(str)
+        self.assertEqual(lst.to_list().sort(),['1','2','3','4','5'].sort())
+        lst = MyHashMap(vItem=[1, 2, 3, 4, 5])
+        lst.map(lambda x:x**2)
+        self.assertEqual(lst.to_list().sort(), [1,4,9,16,25].sort())
+
+
+    def test_reduce(self):
+        lst = MyHashMap(vItem=[1,2,3,4])
+        self.assertEqual(lst.reduce(lambda a,b:a+b, 0),10)
+        test_data=[
+            [],
+            ['a'],
+            ['a','b']
+        ]
+        for e in test_data:
+            lst = MyHashMap()
+            lst.from_list(e)
+            self.assertEqual(lst.reduce(lambda x,_:x+1,0),lst.size())
+
+    @given(st.lists(st.integers()))
+    def test_from_list_to_list_equality(self,a):
+        lst = MyHashMap()
+        lst.from_list(a)
+        b = lst.to_list()
+        self.assertEqual(b.sort(),list(set(a)).sort())
+
+    @given(st.lists(st.integers()))
+    def test_python_len_and_list_size_equality(self,a):
+        lst = MyHashMap(length=len(a)+10, vItem=a)
+        # lst.from_list(a)
+        # print(a,lst.to_list())
+        self.assertEqual(lst.size(),len(a))
+
+
+    def test_iter(self):
+        x =[1,2,3]
+        lst = MyHashMap()
+        tmp=[]
+        for e in lst:
+            tmp.append(e)
+        self.assertEqual(x.sort(),tmp.sort())
+
+
+if __name__=='__main__':
+    unittest.main()
