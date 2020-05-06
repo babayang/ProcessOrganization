7e9f2731c5e5262160faea92cee0f3860b9b0740
yujiabin123 <875711372@qq.com>
2020-05-06 23:38:26 +0800

    second commit

---
+++ Lab1/immutable_version.py
@@ -0,0 +1,64 @@
+from lab1.mutable_version import MyHashMap
+
+def size(vHashMap):
+	return vHashMap.size()
+
+def extend(vHashMap,vValue):
+	newHM=MyHashMap(vHashMap.length,vHashMap.to_list())
+	ncount=0
+	for i in newHM.flag:
+		if(i==1):
+			ncount+=1
+	if(ncount==vHashMap.length):
+		return
+	newHM.insert(vValue)
+	return newHM
+
+def remove(vHashMap,vValue):
+	newHM=MyHashMap(vHashMap.length,vHashMap.to_list())
+	ncount=0
+	for i in range(newHM.length):
+		if(newHM.items[i]==vValue and newHM.flag[i]==1):
+			newHM.items[i]=0
+			newHM.flag[i]=0
+	return newHM
+
+def to_list(vHashMp):
+	return vHashMp.to_list().copy()
+
+def mconcat(a,b):
+	if a!=None and b!=None:
+		newHM=MyHashMap(a.length+b.length)
+		for i in range(a.length):
+			if(a.flag[i]==1):
+				newHM.insert(a.items[i])
+		for i in range(b.length):
+			if(b.flag[i]==1):
+				newHM.insert(b.items[i])
+		return newHM
+	elif a:
+		# return MyHashMap(a.length,a.to_list())
+		return a
+	elif b:
+		# return MyHashMap(b.length,b.to_list())
+		return b
+	else:
+		return None
+
+def mempty():
+	return None
+
+
+
+def iterator(vHashMap):
+	newHM=vHashMap
+	nIndex=0
+	def foo():
+		nonlocal newHM
+		nonlocal nIndex
+		if newHM is None: raise StopIteration
+		temp=newHM.items[nIndex]
+		nIndex+=1
+		return temp
+	return foo
+
---
+++ Lab1/mutable_version.py
@@ -0,0 +1,126 @@
+class MyHashMap(object):
+    def __init__(self, length=16,vItem=None):
+        self.flag=[]
+        self.items = []
+        self.length = length
+        self.n = 0
+        """Initialized to 0"""
+        for i in range(length):
+            self.flag.append(0)
+            self.items.append(0)
+        if vItem!=None:
+
+            for i in range(len(vItem)):
+                self.insert(vItem[i])
+    # def __init__(self, length,vItem,vFlag):
+    #     self.flag=vFlag
+    #     self.items = vItem
+    #     self.length = length
+
+ 
+            
+    def hash(self, key):
+        """Calculate hashkey"""
+        return hash(key) % (self.length)
+
+    def size(self):
+        nCount=0
+        for i in self.flag:
+            if(i!=0):
+                nCount+=1
+        return nCount
+    
+    def to_list(self):
+        res = []
+        for i in range(len(self.flag)):
+            if self.flag[i]==1:
+                res.append(self.items[i])
+        return res
+    
+    def from_list(self,vlist):
+        if len(vlist)>self.length:
+            return 0
+        for i in range(self.length):
+            self.flag[i]=0
+            self.items[i]=0
+        for i in vlist:
+            self.insert(i)
+        return 0
+    
+    def insert(self, value):
+        ncount=0
+        for i in self.flag:
+            if(i==1):
+                ncount+=1
+        if(ncount==self.length):
+            return 
+        HashKey=self.hash(value)
+        if(self.flag[HashKey]==0):
+            self.flag[HashKey]=1
+            self.items[HashKey]=value
+            return 
+        for i in range(HashKey,self.length):
+            if(self.flag[i]==0):
+                self.flag[i]=1
+                self.items[i]=value
+                return 
+        for i in range(0,HashKey):
+            if(self.flag[i]==0):
+                self.flag[i]=1
+                self.items[i]=value
+                return   
+    def remove(self, value):
+        for i in range(self.length):
+            if self.items[i]==value and self.flag[i]==1:
+                self.items[i]=0
+                self.flag[i]=0
+        return
+
+    def find(self,value):
+        HashKey=self.hash(value)
+        if self.items[HashKey]==value and self.flag[HashKey]==1:
+            return True
+        for i in range(HashKey,self.length):
+            if self.items[HashKey]==value and self.flag[HashKey]==1:
+                return True
+        for i in range(0,HashKey):
+            if self.items[HashKey]==value and self.flag[HashKey]==1:
+                 return True
+        return False
+    def filter(self,fun):
+        for i in range(self.length):
+            if self.flag[i]==1 and not fun(self.items[i]):
+                self.flag[i]=0
+                self.items[i]=0
+        return 
+        
+    def map(self,fun):
+         for i in range(self.length):
+            if self.flag[i]==1:
+                self.items[i]=fun(self.items[i])
+        # return
+
+    def reduce(self,fun,init_state):
+        state=init_state
+        for i in range(self.length):
+            if self.flag[i]:
+                state=fun(state, self.items[i])
+        return state
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.n==self.length:
+            raise StopIteration
+        while self.flag[self.n]!=1 and self.n<self.length:
+            self.n+=1
+            if self.n==self.length:
+                raise StopIteration
+        # if self.flag[self.n]:
+        tmp = self.items[self.n]
+        return tmp
+
+
+
+
